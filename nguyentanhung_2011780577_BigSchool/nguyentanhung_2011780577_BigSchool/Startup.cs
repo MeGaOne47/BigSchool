d638b4a14f4e7092811fd12331d7381e5232c978
TanHung <nguyentanhung9a1@gmail.com>
2023-03-11 22:17:48 +0700

    add project file and .gitignore

---
+++ nguyentanhung_2011780577_BigSchool/nguyentanhung_2011780577_BigSchool/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(nguyentanhung_2011780577_BigSchool.Startup))]
+namespace nguyentanhung_2011780577_BigSchool
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ nguyentanhung_2011780577_BigSchool/nguyentanhung_2011780577_BigSchool/Views/ViewModels/CourseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using nguyentanhung_2011780577_BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace nguyentanhung_2011780577_BigSchool.Views.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public byte Category { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+
+        public DateTime GetDateTime()
+        { 
+            return DateTime.Parse(string.Format("{0} {1}", Date, Time));
+        }
+    }
+}
